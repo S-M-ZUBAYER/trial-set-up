be9aa30c98e5dbd635c261fb5674eb1fbc67eb94
S-M-ZUBAYER <smzubayer18@gmail.com>
2023-03-02 14:40:08 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <h1>At last i set up the front end side for react application</h1>
+    </div>
+  );
+}
+
+export default App;
